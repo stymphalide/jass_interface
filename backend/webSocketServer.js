d20fa1310e7c863608cb4d7310b55d5ba1027d8e
stymphalide <angelo.birrer@gmail.com>
2017-11-30 23:15:43 +0100

    Refactored large parts of the wss.

--- backend/webSocketServer.js
+++ backend/webSocketServer.js
@@ -1,5 +1,6 @@
+'use strict'
+
 const WebSocket = require('ws');
-const path = require('path')
 // Create Server
 const wss = new WebSocket.Server({host:"localhost", port: 5000});
 
@@ -33,10 +34,22 @@ wss.on('connection', function connection(ws) {
 
 var init = function(ws, data) {
 	// Have a look at the db
-
 	// Send all games corresponding to data.player
-}
 
+	// A little hack as long as I don't hava a db
+	switch(data.player) {
+		case "pl_1":
+			return ['0', '1', '2', '3'];
+		case "pl_2":
+			return ['0', '1', '2', '3'];
+		case "pl_3":
+			return ['0', '1', '2', '3'];
+		case "pl_4":
+			return ['0', '1', '2', '3'];
+		default:
+			return [];
+	}
+}
 
 var play = function(ws, data) {
 	// Is there a gameId?
@@ -48,17 +61,55 @@ var play = function(ws, data) {
 }
 
 var watch = function(ws, data) {
-	gameId = data.gameId;
+	var gameId = data.gameId;
 	// Check whether the game exists
-	
-	// Fetch the appropriate game data
+	var gameIds = listGameIds();
+	if(gameId in gameIds) {
+		// Fetch the appropriate game data
+		var round = data.round;
+		var turn = data.turn;
+		var filePath = './db/game_' + gameId + '/r' + round + 't' + turn + '.json'
+		const fs = require('fs');
+		fs.readFile(filePath, 'utf8', (err, gameData) => {
+			if(err) {
+				return console.log(err);
+			} else {
+				// Filter unnecessary information
+				console.log("Fetching Game...");
+				var gameState = JSON.parse(gameData);
+				var aP = data.player;
+				gameState.activePlayer = aP;
+				gameState.cardsPlayer = gameState.cards[aP];
+				delete gameState.cards;
+				console.log("Updated Game State");
+				console.log(gameState);
+				// Send the updated game
+				console.log("Send Game");
+				var gameString = JSON.stringify(gameState);
+				ws.send(gameString);
+			}
+		});
+	} else {
+		console.log("No such game found!")
+	}
 
-	// Filter unnecessary information
-
-	// Send the updated game
 }
 
-var listGames = function() {
-	const util = require('util');
-	sys.
+var listGameIds = function() {
+	// Fetch the games
+	// To be replaced by a SQL Query
+	const 
+		{ spawnSync } = require('child_process'),
+		ls = spawnSync( 'ls', [ './db' ]);
+	var data = ls.stdout.toString();
+	var games = data.split("\n");
+	var gameIds = [];
+	for (var i = 0; i < games.length; i++) {
+		if(games[i] == "") {} else {
+			var g = games[i];
+			var l = g.length;
+			gameIds.push(g[l -1]);
+		}
+	}
+	return gameIds;
 }
