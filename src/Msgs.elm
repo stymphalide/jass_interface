ce9a32b41d80ecb4d4efa9d681482a84a0160798
stymphalide <angelo.birrer@gmail.com>
2017-12-18 00:45:22 +0100

    Commented Msgs.elm

--- src/Msgs.elm
+++ src/Msgs.elm
@@ -1,10 +1,26 @@
 module Msgs exposing (..)
+{-
+    @moduledoc
+    Provides two types that are used to send message to the Update module
+    the different types are:
+        OnLocationChange ==> Changes virtual location of the site
+        PlayerChange ==> Any update to the input string at the beginning
+        InitUpdate ==> Updates received from the server consists of a list of games
+        FetchGame ==> Command to send data to the server
+        GameIdUpdate ==> When a Game Id is selected in the init mode
+        GameUpdate ==> When a new gameString is received from the wss (as a response to fetchGame)
+        LobbyUpdate ==> When a new player joins the lobby.
+        SizeUpdated ==> When the window is resized.
+-}
 
+-- Gets the Window size from the browser
+-- Size has width and height
 import Window exposing (Size)
 
 import Models exposing (Mode)
 
 import Game.Model exposing (Player, GameId, GameCoord, Action)
+
 
 type Msg
     = OnLocationChange Mode
@@ -16,6 +32,9 @@ type Msg
     | LobbyUpdate String
     | SizeUpdated Size
 
+-- Allows to store one value in one variable
+-- But it can either be changing as in with every key pressed
+-- Or unchanging when validated
 type InputUpdate 
     = Update String
     | Approve
