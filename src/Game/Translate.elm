4a730efb1eeb1761945a25a383055d132801d35e
stymphalide <angelo.birrer@gmail.com>
2017-12-17 23:13:39 +0100

    Added a dictionary for the colors and numbers from english to german.

--- src/Game/Translate.elm
+++ src/Game/Translate.elm
@@ -1,7 +1,7 @@
 module Game.Translate exposing (..)
 
 import List
-import Tuple
+import Dict
 
 {- TODO Explanation what this does, easier way!!!-}
 
@@ -11,18 +11,14 @@ numbers_english = ["6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
 colors_german = ["Rose", "Schilte", "Eichle", "Schelle"]
 numbers_german = ["6", "7", "8", "9", "10", "Under", "Ober", "K", "As"]
 
+dict_colors_german =
+    List.map2 Dict.singleton colors_english colors_german
+dict_numbers_german =
+    List.map2 Dict.singleton numbers_english numbers_german
+
+-- Is not used (yet)
 colors_french = ["Herz", "Schaufel", "Ecke", "Kreuz"]
 numbers_french = ["6", "7", "8", "9", "10", "Bauer", "Dame", "K", "Ass"]
-
-mappedColors : List (String, String)
-mappedColors =
-    List.map2 (,) colors_english colors_german
-
-mappedNumbers : List (String, String)
-mappedNumbers =
-    List.map2 (,) numbers_english numbers_german
-
-
 
 colorTranslate : String -> String
 colorTranslate color =
@@ -31,25 +27,3 @@ colorTranslate color =
 numberTranslate : String -> String
 numberTranslate number =
     Tuple.second (extractTuple number mappedNumbers)
-
-extractTuple : String -> List(String, String) -> (String, String)
-extractTuple a list = 
-    case List.head (matchingTuple a list) of
-        Just s ->
-            s
-        Nothing ->
-            ("", "")
-
-
-matchingTuple : String  -> List (String, String) -> List (String, String)
-matchingTuple a list =
-    List.filter (isEqualColor a) list
-
-isEqualColor : String  -> ((String, String) -> Bool)
-isEqualColor col =
-    isEqual col
-
-
-isEqual : String -> (String, String) -> Bool
-isEqual a b =
-    a == Tuple.first b
