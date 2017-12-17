456d3940cf372469e1dc1e81e2363e413d402591
stymphalide <angelo.birrer@gmail.com>
2017-12-17 23:46:28 +0100

    Commented Models.elm

--- src/Models.elm
+++ src/Models.elm
@@ -1,18 +1,35 @@
 module Models exposing (..)
+{-
+    @moduledoc
+    Provides the models used on a high level
+    Not so much gameSpecific
+-}
 
 import Window exposing (Size)
 import Game.Model exposing (Game, Player, GameId, Action)
 
+-- This type keeps track of the virtual position of a player.
 type Mode
     = Init
     | Watch GameId Player
     | Play GameId Player
     | Lobby (List Player)
 
+-- For input fields, that are often changing but need to be validated
 type Input a
     = Changing a
     | Constant a
 
+{-
+    The main type of the program.
+    It consists of the virtualPosition or Mode
+    The information about an active game
+    A list of all played games
+    The window size
+    The players Name
+    The gameId related to the active game
+
+-}
 type alias Model =
     { mode : Mode
     , game : Maybe Game
@@ -22,6 +39,11 @@ type alias Model =
     , gameId : Maybe GameId
     }
 
+{-
+    @doc
+    This returns the initial model.
+    Its virtual position is init,
+-}
 initialModel : Model
 initialModel =
     { mode = Init
