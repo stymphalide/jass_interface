bde6504942912e03a4b2f22c6fe0f1d5c24f49bd
stymphalide <angelo.birrer@gmail.com>
2017-12-17 21:37:36 +0100

    Added a viewSvgFunction, that will be used by the table.

--- src/Game/Watch/Card.elm
+++ src/Game/Watch/Card.elm
@@ -1,20 +1,34 @@
 module Game.Watch.Card exposing (..)
 
-import Html exposing (..)
+import Window exposing (Size)
+
+import Html
 import Html.Attributes exposing (src, width)
+import Svg exposing (image)
+import Svg.Attributes exposing (..)
 
 import Msgs exposing (Msg)
-import Globals exposing (imgSourcePath)
+import Globals exposing (imgSourcePath, Position)
 
 import Game.Model exposing (Card, Number, Color)
 import Game.Translate exposing (colorTranslate, numberTranslate)
 
 
 
-viewCard : Card -> Html Msg
-viewCard card  =
-    img [src (getImagePath card.color card.number), width 100] []
+viewSvgCard : Size -> Position -> Card -> Html Msg
+viewSvgCard size pos card  =
+    image
+    [ xlinkHref (getImagePath card.color card.number)
+    , x <| toString pos.x 
+    , y <| toString pos.y
+    , width <| toString size.width
+    , height <| toString size.height
+    ]
+    []
 
+viewCard : Card -> Html Msg
+viewCard card =
+    img [src (getImagePath card.color card.number), width 100] []
 
 
 getImagePath : Color -> Number -> String
