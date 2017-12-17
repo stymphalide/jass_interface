2ab35a699d6fadfceab6a0c97378327e93b0cb8f
stymphalide <angelo.birrer@gmail.com>
2017-12-17 23:36:23 +0100

    Commented the Main.elm file.

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,13 @@
 module Main exposing (..)
-import Html exposing (program)
 
+{-
+    @moduledoc
+    This module keeps the nuts and bolts of the program together.
+    This is accomplished using the Html program.
+-}
+
+-- This is an elm library for working with html tags
+import Html exposing (program)
 
 import Commands exposing (getWindowSize)
 import Models exposing (Model, initialModel)
@@ -11,17 +18,25 @@ import Subscriptions exposing (subscriptions)
 
 
 -- INIT
-
+-- This function returns the initial model and the window size
 init : (Model, Cmd Msg)
 init =
     (initialModel, initialSize)
 
+-- Gets the size from the window.
 initialSize : Cmd Msg
 initialSize =
     getWindowSize
 
 
 -- MAIN
+{-
+    @doc
+    This is the core part of our program.
+    It initialises the model (init)
+    Renders and updates the model (view, update)
+    as well as it listens to subscriptions.
+-}
 main : Program Never Model Msg
 main =
     program
