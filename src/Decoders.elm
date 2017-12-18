1969feffe075396c17c67454720f4b3745aba4c9
stymphalide <angelo.birrer@gmail.com>
2017-12-18 01:06:14 +0100

    Commented Decoders.elm

--- src/Decoders.elm
+++ src/Decoders.elm
@@ -1,6 +1,12 @@
 module Decoders exposing (..)
+{-
+    @moduledoc
+    Decodes Data from the server.
+-}
 
+-- Low Level Decoders from native elm
 import Json.Decode as Decode
+-- High level decoders by NoRedInk.
 import Json.Decode.Pipeline exposing (decode, required)
 
 import Msgs exposing (Msg)
@@ -8,23 +14,26 @@ import Models exposing (Model)
 
 import Game.Model exposing (..)
 
+-- Decodes an array of strings into a list of strings
 gameIdsDecoder : Decode.Decoder (List GameId)
 gameIdsDecoder =
     Decode.list Decode.string
 
+-- Decodes the lobby (necessary is a gameId or players)
 lobbyDecoder : Decode.Decoder Lobby
 lobbyDecoder =
     Decode.oneOf [gameIdDecoder, playersDecoder]
-
+--Helper for the lobbyDecoder if the lobby is full and the game starts this is received
 gameIdDecoder : Decode.Decoder Lobby
 gameIdDecoder =
     decode GameInfo 
     |> required "gameId" Decode.string
-
+-- Helper for lobbyDecoder if the lobby is not full, this is received
 playersDecoder : Decode.Decoder Lobby
 playersDecoder =
     Decode.map Players (Decode.list playerDecoder)
 
+-- Decodes an object into a Game type specified in the Game.Models module
 gameDecoder : Decode.Decoder Game
 gameDecoder =
     decode Game
@@ -38,7 +47,7 @@ gameDecoder =
     |> required "cardsPlayer" (Decode.maybe(Decode.list cardDecoder) )
     |> required "table" tableDecoder
 
-
+-- Decodes into a gameType specified in the Game.Model module.
 gameTypeDecoder : Decode.Decoder GameType
 gameTypeDecoder =
     Decode.maybe(Decode.string)
@@ -65,11 +74,12 @@ gameTypeDecoder =
                         somethingElse ->
                             Decode.fail <| "Unknown theme: " ++ somethingElse
         )
-
+-- Decodes into a player which is bascally a string
 playerDecoder : Decode.Decoder Player
 playerDecoder =
     Decode.string
-    
+
+-- Decodes into a card as specified in the Game.Models module.
 cardDecoder : Decode.Decoder Card
 cardDecoder =
     decode Card
@@ -77,7 +87,7 @@ cardDecoder =
     |> required "number" Decode.string
     
 
-
+-- Decodes into a table as specified in the Game.Models module.
 tableDecoder : Decode.Decoder Table
 tableDecoder =
     decode Table
