84231cf98cf9c7f68506f8cab227745b66b9fcdf
stymphalide <angelo.birrer@gmail.com>
2017-12-19 01:21:31 +0100

    Commented Game.Watch.Groups

--- src/Game/Watch/Groups.elm
+++ src/Game/Watch/Groups.elm
@@ -1,5 +1,8 @@
 module Game.Watch.Groups exposing (..)
-
+{-
+    @moduledoc
+    Renders the Groups in the watch part.
+-}
 import List
 import Html exposing (..)
 import Html.Attributes exposing (class)
@@ -10,7 +13,9 @@ import Game.Model exposing (Group, Card, Player, History)
 
 import Game.Watch.Card exposing (viewCard)
 
-
+-- At the top are the names of the players
+-- next is their points
+-- then the played cards are displayed.
 viewGroup : Maybe Group -> Int -> Html Msg
 viewGroup mGroup width =
     case mGroup of 
@@ -22,15 +27,16 @@ viewGroup mGroup width =
             , h3 [] ["Points: " ++ (toString group.points) |> text]
             , div [class "clearfix"] (viewWonTurns group.wonCards width)
             ]
-
+-- "Group pl_1 pl_2"
 groupName : List Player -> String
 groupName players =
     List.foldr concat "" ("Group" :: players )
-
 concat : String -> String -> String
 concat a b = 
     a ++ " " ++ b
 
+-- Helper function to split the list into two groups
+-- (Better way?)
 unwrapMaybeGroups : Maybe (List Group) -> List Group 
 unwrapMaybeGroups mGroups =
     case mGroups of
@@ -39,7 +45,8 @@ unwrapMaybeGroups mGroups =
         Just groups ->
             groups
 
-
+-- Renders the history of the group.
+-- Just lists them in pairs of 4
 viewWonTurns : History -> Int -> List (Html Msg)
 viewWonTurns mTurns width =
     case mTurns of
@@ -47,7 +54,6 @@ viewWonTurns mTurns width =
             []
         Just wonCards ->
             List.map (viewWonTurn width)  wonCards 
-
 viewWonTurn : Int -> List Card -> Html Msg
 viewWonTurn width cards =
     let
