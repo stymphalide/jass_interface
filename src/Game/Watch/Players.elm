ad8e5f2b468cc5b5c94c36c9635fe5880c5f804b
stymphalide <angelo.birrer@gmail.com>
2017-12-20 23:41:58 +0100

    Added a playerPositions function.

--- src/Game/Watch/Players.elm
+++ src/Game/Watch/Players.elm
@@ -16,8 +16,19 @@ viewSvgPlayers : Position -> Size -> GameCoord -> PlayerInput -> List (Svg msg)
 viewSvgPlayers pos size gameCoord (players, activePlayer, onTurnPlayer) =
 
 playerPositions : Position -> Size -> List Position
-playerPositions position size =
-    [] 
+playerPositions pos size =
+    let
+        pos1 =
+            pos
+        pos2 = 
+            {pos | y = pos.y + size.height}
+
+        pos3 = 
+            {pos | y = pos.y + size.height, x = pos.x + size.width}
+        pos4 =
+            {pos | x = pos.x + size.width}
+    in
+      [pos1, pos2, pos3, pos4] 
 
 
 
@@ -25,44 +36,33 @@ viewPlayers: GameCoord -> PlayerInput -> List (Html Msg)
 viewPlayers gameCoord (players, activePlayer, onTurnPlayer) =
     List.map (viewPlayer gameCoord activePlayer onTurnPlayer) players
 
-
 viewPlayer : GameCoord -> Player -> Player -> Player -> Html Msg
 viewPlayer gameCoord activePlayer onTurnPlayer player =
-    if player == activePlayer then
-        if player == onTurnPlayer then 
-            li [class "inline-block mr1"]
+    li [class "inline-block mr1"]
             [ div [class "col-9"] [text player]
-            , getImage gameCoord player "playerActiveOnTurn"
-            ]
-        else
-            li [class "inline-block mr1"]
-            [ div [class "col-9"] [text player]
-            , getImage gameCoord player "playerActive"
-            ]
-    else 
-        if player == onTurnPlayer then 
-            li [class "inline-block mr1"]
-            [ div [class "col-9"] [text player]
-            , getImage gameCoord player "playerOnTurn"
-            ]
-        else 
-            li [class "inline-block mr1"]
-            [ div [class "col-9"] [text player]
-            , getImage gameCoord player "player"
+            , img 
+                [ src <|  getImageLink activePlayer onTurnPlayer player
+                , width 50
+                , changePlayer gameCoord player
+                ] []
             ]
 
-getImage : GameCoord -> Player ->  String -> Html Msg
-getImage gameCoord player icon =
-    img 
-        [ src (imgSourcePath ++ icon ++"_icon.png")
-        , width 50
-        , changePlayer gameCoord player
-        ] []
-
+getImageLink : Player -> Player -> Player -> String
+getImageLink activePlayer onTurnPlayer player =
+    case player of
+        onTurnPlayer && activePlayer ->
+            imgSourcePath ++ "playerActiveOnTurn_icon.png"
+        activePlayer ->
+            imgSourcePath ++ "activePlayer_icon.png"
+        onTurnPlayer ->
+            imgSourcePath ++ "onTurnPlayer_icon.png"
+        _ ->
+            imgSourcePath ++ "player_icon.png"
 changePlayer :  GameCoord -> Player -> Attribute Msg
 changePlayer gameCoord player  =
     Msgs.FetchGame (Just gameCoord) (Just player) NoAction
     |> onClick
+
 
 viewPlayerCards : Maybe (List Card) -> Int -> List (Html Msg)
 viewPlayerCards mCards width =
