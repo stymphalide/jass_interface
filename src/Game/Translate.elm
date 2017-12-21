effb0f19a5f9a579a74715cd3a37427c8e791e42
stymphalide <angelo.birrer@gmail.com>
2017-12-21 16:33:44 +0100

    Added the the functionality to translate to french in the translate section.

--- src/Game/Translate.elm
+++ src/Game/Translate.elm
@@ -4,6 +4,7 @@ module Game.Translate exposing (..)
     (This is important for rendering purposes)
 -}
 
+import Game.Model exposing (Language(..))
 
 import List
 import Dict exposing (..)
@@ -11,7 +12,6 @@ import Dict exposing (..)
 colors_english : List String
 colors_english = 
     ["hearts", "spades", "diamonds", "clubs"]
-
 numbers_english : List String
 numbers_english = 
     ["6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
@@ -19,10 +19,14 @@ numbers_english =
 colors_german : List String
 colors_german = 
     ["Rose", "Schilte", "Eichle", "Schelle"]
-
 numbers_german : List String
 numbers_german = 
     ["6", "7", "8", "9", "10", "Under", "Ober", "K", "As"]
+
+colors_french : List String
+colors_french = ["Herz", "Schaufel", "Ecke", "Kreuz"]
+numbers_french : List String
+numbers_french = ["6", "7", "8", "9", "10", "Bauer", "Dame", "K", "Ass"]
 
 -- List of Tuples, where each english color is with its associated german color
 mapped_german_colors : List (String, String)
@@ -32,6 +36,12 @@ mapped_german_numbers : List (String, String)
 mapped_german_numbers =
     List.map2 unite numbers_english numbers_german
 
+mapped_french_colors : List (String, String)
+mapped_french_colors =
+    List.map2 unite colors_english colors_french
+mapped_french_numbers : List (String, String)
+mapped_french_numbers =
+    List.map2 unite numbers_english numbers_french
 -- helper function to create a tuple from a list
 -- There might be a built in funciton?
 unite : a -> b -> (a, b)
@@ -46,24 +56,47 @@ dict_numbers_german :  Dict String String
 dict_numbers_german =
     fromList mapped_german_numbers
 
--- Is not used (yet)
-colors_french = ["Herz", "Schaufel", "Ecke", "Kreuz"]
-numbers_french = ["6", "7", "8", "9", "10", "Bauer", "Dame", "K", "Ass"]
+
+-- From the tuples create a dict.
+dict_colors_french : Dict String String
+dict_colors_french =
+    fromList mapped_french_colors
+dict_numbers_french :  Dict String String
+dict_numbers_french =
+    fromList mapped_french_numbers
 
 
 -- Now the lookup is a simple get call.
-colorTranslate : String -> String
-colorTranslate color =
-    case get color dict_colors_german of
-        Nothing ->
-            ""
-        Just color ->
-            color
+colorTranslate : Language -> String -> String
+colorTranslate lang color =
+    case lang of
+        French ->
+            case get color dict_colors_french of
+                Nothing ->
+                    ""
+                Just color ->
+                    color
+        German ->
+            case get color dict_colors_german of
+                Nothing ->
+                    ""
+                Just color ->
+                    color
+            
 
-numberTranslate : String -> String
-numberTranslate number =
-    case get number dict_numbers_german of
-        Nothing ->
-            ""
-        Just number ->
-            number
+numberTranslate : Language -> String -> String
+numberTranslate lang number =
+    case lang of
+        French ->
+                case get number dict_numbers_french of
+                Nothing ->
+                    ""
+                Just number ->
+                    number
+        German ->
+            case get number dict_numbers_german of
+                Nothing ->
+                    ""
+                Just number ->
+                    number
+            
